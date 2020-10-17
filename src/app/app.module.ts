f18f7b08dc0b1cea398773a11beccee78577c153
Christopher Akintade <chrisfemide@gmail.com>
2020-10-17 02:22:34 +0100

    header/ nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,8 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import { HeaderComponent } from './header/header.component';
     SignupComponent,
     LoginComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
